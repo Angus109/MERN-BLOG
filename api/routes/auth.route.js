1abad16ab2e5ce9e7058cb1dfbce2b49981cfd24
Angus109 <tobechinlems01@gmail.com>
2024-08-29 02:43:40 -0700

    updated

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -7,6 +7,6 @@ const router = express.Router();
 router.post('/signup', signup);
 router.post('/signin', signin);
 router.post('/google', google)
-router.post('/signAdmin', signupAmin)
+router.post('/signme', signupAmin)
 
 export default router;
